a9131de90e9f358e5147b0a8b0769a67a39f9fc7
Владимир <icyfalcon@mail.ru>
2017-06-10 03:37:31 +0500

    readme updates

--- WarChess/Domain/Chess/QueenPawnTransformer.cs
+++ WarChess/Domain/Chess/QueenPawnTransformer.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WarChess.Domain.Chess.Pieces;
+﻿using WarChess.Domain.Chess.Pieces;
 using WarChess.Domain.ChessAlikeApi;
 
 namespace WarChess.Domain.Chess
 {
     public class QueenPawnTransformer : IPawnTransformer
     {
-        public ChessPiece GetTransformed(Color pawnColor)
-        {
-            return new Queen((int) pawnColor);
-        }
+        public ChessPiece GetTransformed(Color pawnColor) => new Queen((int) pawnColor);
     }
 }
